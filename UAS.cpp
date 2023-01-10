a8633f800f9513bbd8943345d8ac488e4d56ab78
RidhoMulyawan <114294751+RidhoMulyawan@users.noreply.github.com>
2023-01-10 22:36:15 +0700

    Update UAS.cpp

--- UAS.cpp
+++ UAS.cpp
@@ -1,74 +1,81 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
 
+struct Data
+{
+  int x;
+  int y;
+};
 int main(){
-	float x[100],y[100],xy[100],x2[100],y2[100],korelasi,r;
-	int i,banyak,sigmax,sigmay,sigmaxy,sigmax2,sigmay2;
-	cout<<"Masukkan banyak data: ";
-	cin>> banyak;
-	
-	for (int i = 0; i<banyak; i++){
-	sigmax=0;
-	sigmay=0;
-	sigmaxy=0;
-	sigmax2=0;
-	sigmay2=0;
-	 cout<<"Masukkan nilai x: ";
-	 cin>>x[i];
-	 cout<<"Masukkan nilai y: ";
-	 cin>>y[i];
-	 xy[i]=x[i]*y[i];
-	 cout<<"Hasil kali x dan y"<<" "<< xy[i]<<endl;
-	 x2[i]=x[i]*x[i];
-	 cout<<"Hasil x^2: "<< " "<< x2[i]<<endl;
-	 y2[i]=y[i]*y[i];
-	 cout<<"Hasil y^2: "<< " "<< y2[i]<<endl;
-	 }
-	 for (i=0 ; i<banyak ; i++)
-	 {
-	 sigmax=sigmax+x[i];
-	 sigmay=sigmay+y[i];
-	 sigmaxy=sigmaxy+xy[i];
-	 sigmax2=sigmax2+x2[i];
-	 sigmay2=sigmay2=y2[i];
-	 }
-	 cout<<"total sigma X adalah"<<sigmax<<endl;
-	 cout<<"total sigma Y adalah"<<endl;
-	 cout<<"total sigmaxy"<<sigmaxy<<endl;
-	cout<<"total sigmax^2"<<sigmax2<<endl;
-	cout<<"total sigma y^2"<<sigmay2<<endl;
-	
-	float penyebut1 =sqrt(sigmax2-(sigmax*sigmax));
-	float penyebut2 =sqrt(sigmay2-(sigmay*sigmay));
-	r=penyebut1/penyebut2;
-	
-	if (r<0.09)
- {
- cout << "Hubungan korelasi diabaikan"; cin >> korelasi;
+  system("cls");
+  int n, korelasi,sigmax, sigmay, sigmaxy;
+  float sigmax2, sigmay2,Atas, Bawah1, Bawah2;
+  double koefisien;
+  cout << "Ridho Mulyawan Efendi: ";
+  cout << "152021157 ";
+  cout << "Banyak data: ";
+  cin >> n;
+  double dataxy[n];
+  Data datain[n];
+  vector<Data> berdata;
+  for (int i = 0; i < n; i++){
+    Data data;
+    cout << "masukkan data X-" << i + 1 << " : "; cin >> datain[i].x;
+    while (datain[i].x <= 0) {
+            cout << "Nilai x tidak boleh kurang dari 0. Masukkan Nilai x: ";cin >> datain[i].x;
+    }
+    cout << "masukkan data Y-" << i + 1 << " : ";cin >> datain[i].y;
+    while (datain[i].y <= 0) {
+            cout << "Nilai y tidak boleh kurang dari 0. Masukkan Nilai y: ";cin >> datain[i].y;
+    }
+    berdata.push_back(data);
+    sigmax += datain[i].x;
+    sigmay += datain[i].y;
+    dataxy[i] = datain[i].x * datain[i].y;
+    sigmaxy += dataxy[i];
+  }
+  sigmax2 = pow(sigmax, 2);
+  sigmay2 = pow(sigmay, 2);
+  Atas = (n * sigmaxy) - (sigmax * sigmay);
+  Bawah1 = sqrt((n * sigmax2) - (pow(sigmax, 2)));
+  Bawah1 = sqrt((n * sigmay2) - (pow(sigmay, 2)));
+  double r = Atas / (Bawah1 * Bawah2);
+  koefisien = pow(r, 2) * (1 / 100);
+  cout<<"Hasil Nilai: "<<endl;
+  cout << "sigma x = " << sigmax << endl;
+  cout << "sigma y = " << sigmay << endl;
+  cout << "sigmaXY = " << sigmaxy << endl;
+  cout << "sigmaX2 = " << sigmax2 << endl;
+  cout << "sigmaY2 = " << sigmay2 << endl;
+  cout << "a. Nilai Korelasi r = " << Atas << " / " << Bawah1 * Bawah2 << " = " << r << endl;
+  cout << "b. Nilai koefisien determinasi = " << koefisien << endl;
+
+if (r<=0.09){
+ cout << "c. Hubungan korelasinya diabaikan"; 
  }
- if (r<0.29)
- {
- cout << "Hubungan korelasi rendah"; cin >> korelasi;
+ else if (r<=0.29){
+ cout << "c. Hubungan korelasinya rendah"; 
  }
- if (r<0.49)
- {
- cout << "Hubungan korelasi moderat"; cin >> korelasi;
+ else if (r<=0.49){
+ cout << "c. Hubungan korelasinya moderat"; 
  }
- if (r<0.70)
- {
- cout << "Hubungan korelasi sedang"; cin >> korelasi;
+ else if (r<=0.70){
+ cout << "c. Hubungan korelasinya sedang"; 
  }
- if (r>0.70)
- {
-    cout << "Hubungan korelasi sangat kuat"; cin >> korelasi;
- }	 
+ else if (r>0.70){
+    cout << "c. Hubungan korelasinya sangat kuat"; 
+ }
+    cout << endl;
+  if (r == 0){
+    cout << "f. Tidak Ada Hubungan Antara variabel X dan Y adalah Positif" << endl;
+  }
+  else if (r > 0){
+    cout << "d. Hubungan Antara variabel X dan Y adalah Positif" << endl;
+  }
+  else if (r < 0){
+    cout << "e. Hubungan Antara variabel X dan Y adalah Negatif" << endl;
+  }
+  return 0;
 }
-	 
- 
-
-
-
-
-
-
